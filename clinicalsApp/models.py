176c1bc5fc0e90ea2e1d2a3c44374e35142175fd
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-05-08 22:33:25 +0530

    Models Created

--- clinicalsApp/models.py
+++ clinicalsApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+class ClinicalData(models.Model):
+    componentName = models.CharField(max_length=30)
+    componentValue = models.CharField(max_length=30)
+    measuredDateTime = models.DateTimeField()
+    patient = models.ForeignKey(Patient,on_delete=models.CASCADE)
