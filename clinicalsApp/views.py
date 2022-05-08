b6195fc67919c405c0713d93aea4531a635a2829
AadharT1202 <aadhar.toshniwal12@gmail.com>
2022-05-08 22:45:52 +0530

    Patient Views Created

--- clinicalsApp/views.py
+++ clinicalsApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from clinicalsApp.models import Patient, ClinicalData
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
+
+class PatientListView(ListView):
+    model = Patient
+
+class PatientCreateView(CreateView):
+    model = Patient
+    success_url = reverse_lazy('index')
+    fields = ('firstName','lastName','age')
+
+class PatientUpdateView(UpdateView):
+    model = Patient
+    success_url = reverse_lazy('index')
+    fields = ('firstName','lastName','age')
+
+class PatientDeleteView(DeleteView):
+    model = Patient
+    success_url = reverse_lazy('index')
